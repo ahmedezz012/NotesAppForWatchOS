c7750c8cab6d49cd281732411248ae62b2e0445f
Ahmed Ezz <ahmed_ezz2021995@yahoo.com>
2023-10-10 01:30:14 +0300

    Initial Commit

---
+++ notes Watch App/notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesApp.swift
+//  notes Watch App
+//
+//  Created by Ahmed Ezz on 10/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
